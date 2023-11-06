d7c9271706257137129ac193eaae020ca1c2d3b9
Illia <1304illia@gmail.com>
2023-11-06 09:55:53 +0200

    add ICanHazDadJoke.NET output random joke

--- Lesson24/Program.cs
+++ Lesson24/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using ICanHazDadJoke.NET;
+
+var libraryName = "Dad jokes";
+var contactUri = "https://github.com/mattleibow/ICanHazDadJoke.NET";
+var client = new DadJokeClient(libraryName, contactUri);
+
+string joke = await client.GetRandomJokeStringAsync();
+Console.WriteLine(joke);
